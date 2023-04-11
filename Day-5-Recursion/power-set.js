71903da07d27388b1af4026fbe56ac1315dc1311
TahereGmi <mstgholami@gmail.com>
2023-04-11 23:04:21 +0330

    Add day 6 files -string

--- Day-5-Recursion/power-set.js
+++ Day-5-Recursion/power-set.js
@@ -1,3 +1,4 @@
+
 // Given an integer array of unique elements, return all possible subsets (the power set). 
 // The solution set must not contain duplicate subsets. 
 // Return the solution in any order. 
---
+++ Day-6-String/Palindrome.js
@@ -0,0 +1,1 @@
+// You are given a string. Write a function to check whether the string is a palindrome or not.
